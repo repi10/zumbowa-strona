6e59b20dfaf25e32b169006b101622b9c9e4d5e0
Dawid P <dawid@twojpomysl.it>
2022-07-12 11:45:44 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const compression = require('compression')
+app.use(compression)
 app.use(express.static(__dirname + '/dist/my-app'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
