a0bba384cb3090af2444640284858b269da76ded
Dawid P <dawid@twojpomysl.it>
2022-07-19 01:45:38 +0200

    Fixed host

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const compression = require('compression')
-app.use(compression)
 app.use(express.static(__dirname + '/dist/my-app'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
